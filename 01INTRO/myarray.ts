5c5b8ef8e7acd8afd1bc4eaa8dfb89774e34ffd5
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-11 17:06:26 +0530

    array in typescript

---
+++ 01INTRO/myarray.ts
@@ -0,0 +1,17 @@
+const superHeroes:string[] = [];//array of strings only
+superHeroes.push("spidernam");
+
+
+const heroPowers:number[] = [];//array of numbers only
+heroPowers.push(4);
+
+
+
+const heroPowers1:Array<number> = [];//array of numbers only
+heroPowers1.push(5);
+
+type User = {
+    name:string, isActive:boolean
+}
+const allUsers:User[]=[];
+allUsers.push({name:"Jitin",isActive:true});
