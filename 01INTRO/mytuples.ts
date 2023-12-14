ba94e11aaaa7dc15f78a7797193c6d13ed461846
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-14 18:07:13 +0530

    tuples

---
+++ 01INTRO/mytuples.ts
@@ -0,0 +1,14 @@
+let tUser:[number,string,boolean];//for preserving order
+
+tUser = [17,"jj",true];//allowed
+///tUser = ["jj",true,17];//not allowed
+
+let rgb:[number,number,number];
+
+rgb = [255,202,126];//allowed
+//rgb = [255,147,789,0.5];//not allowed since only 3 is defined
+
+
+type User1 =[number,string];
+
+const nUser:User1 = [123,"jitin"];
