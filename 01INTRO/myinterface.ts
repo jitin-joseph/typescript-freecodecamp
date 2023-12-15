294b155880b890ff2e01c4f5fa2b06ae85194ecb
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-15 17:00:35 +0530

    interface extends

--- 01INTRO/myinterface.ts
+++ 01INTRO/myinterface.ts
@@ -8,6 +8,18 @@ interface User {
     getCoupon(couponname:string,value:number):number
 }
 
+//we can also reopen interface to add more items later on
+
+interface User{
+    githubToken : string
+}
+
+
+interface Admin extends User{
+
+    role:"admin"|"TA"|"Learner"
+}
+
 const jitin:User = {
     dbId:333,
     userId:343,
@@ -19,6 +31,27 @@ const jitin:User = {
     getCoupon:(name:"jitin10", offer:10)=> {
         return 10
     },
+    githubToken:"sdsdsaf423423423"
 }
 
+
+
 //jitin.dbId = 333;//error because it is a readonly
+
+
+const jitinAdmin:Admin = {
+    dbId:333,
+    userId:343,
+    name:"jitin",
+    email:'jitin@jitin.com',
+    startTrial:()=>{
+        return "startted trial"
+    },
+    getCoupon:(name:"jitin10", offer:10)=> {
+        return 10
+    },
+    githubToken:"sdsdsaf423423423",
+    role:"admin"
+}
+
+
