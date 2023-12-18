9bca347c9ca54acb68031cc05413e7c714ffdae5
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-18 12:55:50 +0530

    classes

---
+++ 03PURETS/src/classes.ts
@@ -0,0 +1,12 @@
+class User {
+    name: string;
+    email: string;
+    isActive: boolean=false;
+    city:string="Kochi";
+    constructor(email:string,name:string){
+        this.email= email;
+        this.name = name;
+    }
+}
+
+const jitin = new User("jitin","j@j.com");
